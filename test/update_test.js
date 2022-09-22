28759a1624607dfaa8574efd41ed82336fa52e99
Scott <ssmythw@outlook.com>
2022-09-22 11:07:21 -0400

    working on post schema

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe("Updating records", () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(() => {
       done();
     });
@@ -46,5 +46,14 @@ describe("Updating records", () => {
 
   it("A model class can find a record with an Id and update", (done) => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+
+  it("Increment post count by 1", (done) => {
+    User.updateMany({ name: "Joe" }, { $inc: { postCount: 1 } }).then(() => {
+      User.findOne({ name: "Joe" }).then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
+    });
   });
 });
